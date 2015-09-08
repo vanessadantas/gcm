352e0a25311a1490bebeef88179a4510ade40bb9
vanessadantas <dantasp.vanessa@gmail.com>
2015-09-08 10:51:41 -0300

    Classe SistemaBeam refatorada.

--- src/main/java/gcm/web/SistemaBean.java
+++ src/main/java/gcm/web/SistemaBean.java
@@ -2,6 +2,7 @@ package gcm.web;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,11 +13,13 @@ import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
+import gcm.aplicacao.CrudService;
 import gcm.dominio.Arquitetura;
 import gcm.dominio.Framework;
 import gcm.dominio.Linguagem;
 import gcm.dominio.Responsavel;
 import gcm.dominio.Sistema;
+import gcm.infra.CrudServiceImpl;
 import gcm.infra.JPAUtil;
 
 @ManagedBean
@@ -34,41 +37,41 @@ public class SistemaBean {
 	private Sistema sistema = new Sistema();
 	public Arquitetura arquitetura = new Arquitetura();
 	public Responsavel responsavel = new Responsavel();
+	public CrudService<Sistema> crudService = new CrudServiceImpl<>(Sistema.class);
+	public CrudService<Arquitetura> crudServiceArquitetura = new CrudServiceImpl<>(Arquitetura.class);
+	public CrudService<Responsavel> crudServiceResponsavel = new CrudServiceImpl<>(Responsavel.class);
+		
 	
 	// preencher os valos da combobox de arquitetura
 	public void pesquisarArquitetura() {
 		if (linguagemSelecionada != "") {
-			EntityManager em = JPAUtil.getEntityManager();
-			TypedQuery<Arquitetura> query = em.createNamedQuery(Arquitetura.PESQUISAR_POR_LINGUAGEM, Arquitetura.class);
-			query.setParameter("linguagem", Linguagem.valueOf(linguagemSelecionada));
-			arquiteturas = query.getResultList();
+			Map<String, Object> parametros = new HashMap<>();
+			parametros.put("linguagem", Linguagem.valueOf(linguagemSelecionada));
+			arquiteturas = crudServiceArquitetura.pesquisarPorNamedQuery(Arquitetura.PESQUISAR_POR_LINGUAGEM,parametros);
 		}
 	}
 	// preencher os valos da combobox de responsável
 
 	public void listarResponsaveis() {
-		EntityManager em = JPAUtil.getEntityManager();
-		TypedQuery<Responsavel> query = em.createNamedQuery(Responsavel.LISTAR_TODOS, Responsavel.class);
-		responsaveis = query.getResultList();
+		responsaveis = crudServiceResponsavel.pesquisarPorNamedQuery(Responsavel.LISTAR_TODOS);
 	}
-	// selecionar responsável da combobox
 
+	// selecionar responsável da combobox
 	public void adicionarResponsavel(){
 		responsaveisSelecionados.add(responsavelSelecionado);
 		System.out.println(responsaveisSelecionados);
 	}
-	// remover responsável da lista
 
+	// remover responsável da lista
 	public void removerResponsavel(Responsavel responsavel) {
 		responsaveisSelecionados.remove(responsavel);
 	}
 
 	// Usado para cadastro
 	public String salvar() {
-		EntityManager em = JPAUtil.getEntityManager();
 		sistema.getResponsaveis().clear();
 		sistema.getResponsaveis().addAll(responsaveisSelecionados);
-		em.merge(sistema);
+		crudService.salvar(sistema);
 		FacesMessage msg = new FacesMessage("Sistema cadastrado com sucesso");
 		FacesContext.getCurrentInstance().addMessage(null, msg);
 		FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
@@ -77,10 +80,9 @@ public class SistemaBean {
 
 	// Usado para pesquisar
 	public String pesquisarSistema() {
-		EntityManager em = JPAUtil.getEntityManager();
-		TypedQuery<Sistema> query = em.createNamedQuery(Sistema.PESQUISAR_POR_NOME, Sistema.class);
-		query.setParameter("nome", "%" + nomeSistema.toUpperCase() + "%");
-		sistemas = query.getResultList();
+		Map<String, Object> parametros = new HashMap<>();
+		parametros.put("nome", "%" + nomeSistema.toUpperCase() + "%");
+		sistemas = crudService.pesquisarPorNamedQuery(Sistema.PESQUISAR_POR_NOME, parametros);
 		if (sistemas.isEmpty()) {
 			FacesMessage msg = new FacesMessage("Não foi encontrado sistema");
 			FacesContext.getCurrentInstance().addMessage(null, msg);
@@ -90,15 +92,11 @@ public class SistemaBean {
 
 	// Usado para alterar
 	public SistemaBean() {
-		// Verifica se foi passada um parametro via querystring com o nome de
-		// "id"
-		// Em caso positivo, trata-se de uma alteração do framework
 		Map<String, String> parametros = FacesContext.getCurrentInstance().getExternalContext()
 				.getRequestParameterMap();
 		String parametroId = parametros.get("id");
 		if (parametroId != null) {
-			EntityManager em = JPAUtil.getEntityManager();
-			sistema = em.find(Sistema.class, Long.valueOf(parametroId));
+			sistema = crudService.buscarPorId(Long.valueOf(parametroId));
 			try {
 				linguagemSelecionada = sistema.getArquitetura().getFrameworks().iterator().next().getLinguagem().toString();
 			} catch (NullPointerException npe) {
@@ -113,9 +111,7 @@ public class SistemaBean {
 
 	// método de exclusão
 	public void excluir(Long idSistema) {
-		EntityManager em = JPAUtil.getEntityManager();
-		Sistema sistema = em.find(Sistema.class, idSistema);
-		em.remove(sistema);
+		crudService.excluir(idSistema);
 		pesquisarSistema();
 	}
 
